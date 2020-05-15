dac015c10750518c601d8adb18c48daafd501792
alwayswannafly <15007159069@163.com>
2020-05-15 17:38:09 +0800

    标题栏和菜单栏实现过渡动画显示与隐藏

--- ebook-demo/src/Ebook.vue
+++ ebook-demo/src/Ebook.vue
@@ -1,25 +1,44 @@
 <template>
   <div class="ebook">
-    <div class="title-wrapper">
-      <div class="left">
-        <span class="icon-arrow-left2 icon"></span>
-      </div>
-      <div class="right">
-        <div class="icon-wrapper">
-          <span class="icon-cart icon"></span>
-          <span class="icon-user icon"></span>
-          <span class="icon-menu icon"></span>
+    <transition name="slide-down">
+      <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="left">
+          <span class="icon-arrow-left2 icon"></span>
+        </div>
+        <div class="right">
+          <div class="icon-wrapper">
+            <span class="icon-cart icon"></span>
+            <span class="icon-user icon"></span>
+            <span class="icon-cogs icon"></span>
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
+
     <div class="read-wrapper">
       <div id="read"></div>
       <div class="mask">
         <div class="left" @click="prevPage"></div>
-        <div class="center"></div>
+        <div class="center" @click="toggleTitleAndMenu"></div>
         <div class="right" @click="nextPage"></div>
       </div>
     </div>
+    <transition name="slide-up">
+      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="icon-wrapper">
+          <span class="icon-menu icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-target icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-brightness-contrast icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-a icon">A</span>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -28,7 +47,9 @@ import Epub from "epubjs";
 const DOWNLOAD_URL = "/static/中国绘画史.epub";
 export default {
   data() {
-    return {};
+    return {
+      ifTitleAndMenuShow: false
+    };
   },
   mounted() {
     this.showEpub();
@@ -56,6 +77,9 @@ export default {
       if (this.rendition) {
         this.rendition.next();
       }
+    },
+    toggleTitleAndMenu() {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow;
     }
   }
 };
@@ -104,15 +128,31 @@ export default {
       display: flex;
       justify-content: flex-end;
       .icon-wrapper {
-        // flex: 0 0 px2rem(80);
+        flex: 0 0 px2rem(80);
         @include center;
-        .icon{
+        .icon {
           margin: 0 px2rem(3);
         }
-        .icon-cart{
+        .icon-cart {
           font-size: px2rem(18);
         }
       }
+    }
+  }
+  .menu-wrapper {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 101;
+    width: 100%;
+    height: px2rem(48);
+    background-color: #fff;
+    display: flex;
+    display: flex;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+    .icon-wrapper {
+      flex: 1;
+      @include center;
     }
   }
 }
