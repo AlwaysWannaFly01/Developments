ab89cd231c2a0ecc0859b60db50ae997d1147160
alwayswannafly <15007159069@163.com>
2020-05-15 15:30:03 +0800

    电子书的解析和渲染，eslint取消校验的放啊

---
+++ ebook-demo/src/Ebook.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Epub from "epubjs";
+const DOWNLOAD_URL = "/static/中国绘画史.epub";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.showEpub();
+  },
+  methods: {
+    showEpub() {
+      /* 生成book */
+      this.book = new Epub(DOWNLOAD_URL);
+      // console.log(this.book);
+      /*  生成Renition ，通过Book.renderTo */
+      this.rendition = this.book.renderTo("read", {
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+
+      /* 通过Rendition.display渲染电子书 */
+      this.rendition.display();
+    }
+  }
+};
+</script>
+<style lang='scss' scoped>
+@import "assets/styles/global";
+</style>
