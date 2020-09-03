0a5dcb4ed70efb73de4d7e5161ad192b5323ac6a
alwayswannafly <15007159069@163.com>
2020-09-03 15:36:34 +0800

    初始话配置，移动端适配，vant组件按需引入

---
+++ fast-appraisal/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Button } from 'vant';
+
+Vue.use(Button);
+export default {
+  name: "Login",
+};
+</script>
+
+<style>
+</style>
