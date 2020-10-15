7bd93fc6a67cf04ea337db29aab80f2baac28087
alwayswannafly <15007159069@163.com>
2020-10-15 18:05:36 +0800

    商品列表卡片切换

--- jiangxia-mall/src/views/category/goodsList.vue
+++ jiangxia-mall/src/views/category/goodsList.vue
@@ -25,9 +25,10 @@
 				</van-button>
 			</div>
 			<div class="item">
-				<van-button type="default" size="mini">
+				<van-button type="default" size="mini" @click="priceOrderBy">
 					价格
 				</van-button>
+
 				<img src="../../assets/images/horn.png">
 			</div>
 			<div class="item">
@@ -46,8 +47,13 @@
 		<div class="container" :style="mainHeight">
 			<van-list
 				v-if="displayGrid"
+				v-model="loading"
+				:finished="finished"
 				class="grid"
 				:style="mainHeight"
+				finished-text="没有更多了"
+				@load="onLoad"
+				offset="30"
 			>
 				<van-cell v-for="(item,index) in searchList" :key="index">
 					<img :src="item.goodsImg">
@@ -63,6 +69,7 @@
 			</van-list>
 
 			<van-list
+				v-else
 				v-model="loading"
 				:finished="finished"
 				finished-text="没有更多了"
@@ -70,7 +77,6 @@
 				class="w100"
 				@load="onLoad"
 				offset="30"
-				v-else
 			>
 				<van-cell v-for="(item,index) in searchList" :key="index">
 					<img :src="item.goodsImg">
@@ -103,17 +109,16 @@ export default {
 			searchValue: '',
 			searchList: [],
 			loading: false,
-			finished: true,
+			finished: false,
 			queryParams: {
-				page: 1
+				page: 0,
+				priceOrder: -1
 			},
 			displayGrid: true
 		}
 	},
 	beforeMount() {
 		this.searchValue = this.$route.query.searchValue;
-		this.init(this.searchValue);
-
 		this.deviceHeight = window.innerHeight;
 		this.mainHeight = {
 			height: window.innerHeight - 54 - 41 + "px",
@@ -121,25 +126,12 @@ export default {
 	},
 
 	methods: {
-		async init(param) {
-			const result = await this.search(param);
-			// console.log(result)
-			if (result.status === 1) {
-				result.data.data.map(item => {
-					if (_.startsWith(item.goodsImg, "http")) {
-						return;
-					} else {
-						item.goodsImg = "http://youyoujiang.com/" + item.goodsImg;
-					}
-				})
-				this.searchList = result.data.data;
-			}
-		},
 		search(param) {
 			return new Promise((resolve, reject) => {
 				Request("main", "weapp/goods/goodslistbycondition", "post", {
 					keyword: param ? param : '',
-					page: this.queryParams.page
+					page: this.queryParams.page,
+					desc: this.queryParams.priceOrder
 				}).then(res => {
 					if (res.status === 1) {
 						resolve(res)
@@ -151,40 +143,33 @@ export default {
 			})
 		},
 		async onSearch() {
-			const {searchValue} = this;
-			if (!searchValue) {
-				this.searchValue = '';
-				let isTrue = await this.refresh();
-				// console.log(isTrue);
-				if (isTrue) {
-					const result = await this.search();
-					console.log(result)
-					if (result.status === 1) {
-						result.data.data.map(item => {
-							if (_.startsWith(item.goodsImg, "http")) {
-								return;
-							} else {
-								item.goodsImg = "http://youyoujiang.com/" + item.goodsImg;
-							}
-						})
-						if (parseInt(result.data.current_page) === result.data.last_page) {
-							this.finished = true;
-							this.loading = false;
+			let isTrue = await this.refresh();
+			if (isTrue) {
+				let result = await this.search(this.searchValue);
+				if (result.status === 1) {
+					result.data.data.map(item => {
+						if (_.startsWith(item.goodsImg, "http")) {
+							return;
 						} else {
-							this.searchList = result.data.data;
-							this.finished = false;
-							this.loading = false;
+							item.goodsImg = "http://youyoujiang.com/" + item.goodsImg;
 						}
+					})
+					if (parseInt(result.data.current_page) < result.data.last_page) {
+						this.finished = false;
+						this.loading = false;
+						this.searchList = this.searchList.concat(result.data.data);
+					} else if (parseInt(result.data.current_page) === result.data.last_page) {
+						this.finished = true;
+						this.loading = false;
+						this.searchList = this.searchList.concat(result.data.data);
 					}
 				}
-			} else {
-				console.log(searchValue)
 			}
 		},
 		async onLoad() {
 			this.queryParams.page++;
 			this.loading = true;
-			const result = await this.search();
+			const result = await this.search(this.searchValue);
 			console.log(result)
 			if (result.status === 1) {
 				result.data.data.map(item => {
@@ -194,26 +179,57 @@ export default {
 						item.goodsImg = "http://youyoujiang.com/" + item.goodsImg;
 					}
 				})
-				if (parseInt(result.data.current_page) === result.data.last_page) {
-					this.finished = true;
-					this.loading = false;
-				} else {
+				if (parseInt(result.data.current_page) < result.data.last_page) {
 					setTimeout(() => {
 						this.loading = false;
 						this.searchList = this.searchList.concat(result.data.data);
 						this.finished = false;
 					}, 600)
+				} else if (parseInt(result.data.current_page) === result.data.last_page) {
+					this.loading = false;
+					this.finished = true;
+					this.searchList = this.searchList.concat(result.data.data);
 				}
 			}
 		},
 		clickSearch() {
 			this.displayGrid = !this.displayGrid;
 		},
+		async priceOrderBy() {
+			if (this.queryParams.priceOrder < 1) {
+				this.queryParams.priceOrder++;
+			} else {
+				this.queryParams.priceOrder = -1;
+			}
+			console.log(this.queryParams.priceOrder);
+			let isTrue = await this.refresh();
+			if (isTrue) {
+				let result = await this.search();
+				console.log(result)
+				if (result.status === 1) {
+					result.data.data.map(item => {
+						if (_.startsWith(item.goodsImg, "http")) {
+							return;
+						} else {
+							item.goodsImg = "http://youyoujiang.com/" + item.goodsImg;
+						}
+					})
+					if (parseInt(result.data.current_page) < result.data.last_page) {
+						this.finished = false;
+						this.loading = false;
+						this.searchList = this.searchList.concat(result.data.data);
+					} else if (parseInt(result.data.current_page) === result.data.last_page) {
+						this.finished = true;
+						this.loading = false;
+						this.searchList = this.searchList.concat(result.data.data);
+					}
+				}
+			}
+
+		},
 		async refresh() {
 			return new Promise((resolve, reject) => {
-				this.queryParams = {
-					page: 1
-				}
+				this.queryParams.page = 1;
 				this.searchList = [];
 				this.loading = true;
 				this.finished = false;
@@ -309,6 +325,7 @@ export default {
 						strong {
 							font-size: 14px;
 							color: #7abb56;
+							font-weight: 700;
 						}
 
 						p {
@@ -334,26 +351,82 @@ export default {
 			display: flex;
 			flex-wrap: wrap;
 			width: 100%;
-			padding: px2rem(10);
+			padding: px2rem(10) px2rem(10) px2rem(0);
 			box-sizing: border-box;
-			background-color: #ebedf0;
+			background-color: rgba(249, 249, 249, .8);
 			overflow-y: auto;
+
 			.van-cell {
 				border-radius: px2rem(4);
+
 				&:nth-child(2n-1) {
 					margin-right: px2rem(11);
 				}
+
 				margin-bottom: px2rem(10);
 				width: px2rem(172);
+				padding: px2rem(10);
+				box-sizing: border-box;
+
+				&::after {
+					right: 0;
+					left: 0;
+					border-bottom: none;
+				}
 
 				.van-cell__value {
 					display: flex;
 					flex-direction: column;
 					align-items: center;
+
 					img {
-						width: px2rem(80);
+						width: px2rem(152);
+						height: px2rem(152);
+					}
+
+					div {
+						flex: 1;
+						width: 100%;
+						margin-top: px2rem(10);
+						display: flex;
+						flex-direction: column;
+						justify-content: space-between;
+						h3 {
+							font-size: 16px;
+							font-weight: 700;
+						}
+
+						strong {
+							font-size: 14px;
+							color: #7abb56;
+							font-weight: 700;
+						}
+
+						p {
+							display: flex;
+							justify-content: space-between;
+
+							del {
+								color: #afafaf;
+								font-size: 12px;
+							}
+
+							span {
+								color: #afafaf;
+								font-size: 12px;
+							}
+						}
 					}
 				}
+			}
+
+			.van-list__finished-text {
+				width: 100%;
+			}
+
+			.van-list__loading {
+				width: 100%;
+				margin-top: px2rem(-10);
 			}
 		}
 	}
